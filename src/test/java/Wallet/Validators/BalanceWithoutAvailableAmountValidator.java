c1d1f525e213dba5ef3febcadf3889f3271210cd
leonardo <leowille97@gmail.com>
2021-06-16 17:20:52 -0300

    feat: Add Wallet API response validators

---
+++ src/test/java/Wallet/Validators/BalanceWithoutAvailableAmountValidator.java
@@ -0,0 +1,20 @@
+package Wallet.Validators;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+
+public class BalanceWithoutAvailableAmountValidator implements Validator {
+
+	@Override
+	public boolean validate(Response response) {
+		JsonObject responseBody = new Gson().fromJson(response.getBody().asString(), JsonObject.class);
+		Assertions.assertEquals(0, responseBody.get("amount").getAsJsonObject().get("available").getAsDouble());
+
+		return true;
+	}
+}
+
+
+
