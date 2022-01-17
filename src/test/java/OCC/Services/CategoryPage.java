83f6706423194cd78118976adebe1bafeed9aa7e
juliano.schons <julianoschons@hotmail.com>
2022-01-17 13:42:48 -0300

    Criado classe Fixture para alimentar dados busca do request

--- src/test/java/OCC/Services/CategoryPage.java
+++ src/test/java/OCC/Services/CategoryPage.java
@@ -3,6 +3,7 @@ package OCC.Services;
 import Common.Validators.StatusCodeOKValidator;
 import Common.Validators.Validator;
 import OCC.DTOs.ProductCategorySearchPageRequestDTO;
+import OCC.Fixtures.ProductCategorySearchPageRequestDTOFixture;
 import OCC.Utils.APIClient;
 import OCC.Utils.Utils;
 import OCC.Validators.ProductImagesValidator;
@@ -19,23 +20,16 @@ import java.util.List;
 public class CategoryPage {
 
     public static void GET_searchProductsByCode() {
-        ProductCategorySearchPageRequestDTO productCategorySearchPageRequestDTO = new ProductCategorySearchPageRequestDTO();
-        productCategorySearchPageRequestDTO.setFields("FULL");
-        productCategorySearchPageRequestDTO.setQuery("3510500180001U");
-        productCategorySearchPageRequestDTO.setCurrentPage("0");
 
-        Response response = APIClient.GET_product(productCategorySearchPageRequestDTO);
+        Response response = APIClient.GET_product(new ProductCategorySearchPageRequestDTOFixture().defaultRequestByCode().build());
 
         List<Validator> validators = Arrays.asList(new StatusCodeOKValidator(), new ProductNameValidator(), new ProductValueValidator(), new ProductImagesValidator());
         Assertions.assertTrue(validators.stream().allMatch(validator -> validator.validate(response)));
     }
 
     public static void GET_searchProductsWithoutFilters() {
-        ProductCategorySearchPageRequestDTO productCategorySearchPageRequestDTO = new ProductCategorySearchPageRequestDTO();
-        productCategorySearchPageRequestDTO.setFields("FULL");
-        productCategorySearchPageRequestDTO.setCurrentPage("0");
 
-        Response response = APIClient.GET_product(productCategorySearchPageRequestDTO);
+        Response response = APIClient.GET_product(new ProductCategorySearchPageRequestDTOFixture().defaultRequestFull().build());
 
         List<Validator> validators = Arrays.asList(new StatusCodeOKValidator(), new ProductImagesValidator(), new ProductNameValidator(), new ProductValueValidator());
         Assertions.assertTrue(validators.stream().allMatch(validator -> validator.validate(response)));
